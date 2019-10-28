f785f7688ac4e7d4fb2c128bb6599361c67c895c
Screek1 <Sema228779@yandex.ru>
2019-10-28 11:20:19 +0300

    fix bug

--- resources/js/views/Dashboard/Pages/Edit.vue
+++ resources/js/views/Dashboard/Pages/Edit.vue
@@ -43,6 +43,7 @@
                         if (response.data.is_admin === false) {
                             location = ('/courses')
                         } else {
+                            console.log(response.data)
                             this.is_admin = response.data.is_admin
                             this.users = response.data.users
                             this.roles = response.data.roles
